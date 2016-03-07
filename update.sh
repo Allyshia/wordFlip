2736885cf1e89d5330af7ced0b96db5786b8dcbd
asewdat <allyshias@gmail.com>
2016-03-06 20:01:03 -0500

    Add update script to be run on prod server.

---
+++ update.sh
@@ -0,0 +1,11 @@
+# Pull latest word-flip Docker image
+docker pull asewdat/word-flip
+
+# Kill existing word-flip app if running
+docker kill word-flip-app
+
+# Remove word-flip-app container if exists
+docker kill word-flip-app
+
+# Start word-flip-app using latest image
+docker run -p 80:3000 --name word-flip-app --link mongo-img-0:mongo -d asewdat/word-flip
